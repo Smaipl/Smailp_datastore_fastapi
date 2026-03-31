b6dffc55978dcb27392956f2eb4d050ca00505a0
wsnk <wsnk00@yandex.com>
2026-04-01 00:32:02 +0300

    Fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,6 @@ PROJECT_ROOT="$(realpath "$(dirname "$0")/..")"
 
 cd "$PROJECT_ROOT"
 exec uvicorn app.main:app \
-    --host localhost \
+    --host 0.0.0.0 \
     --port 8000 \
     --log-config "$PROJECT_ROOT/logging.yaml"
